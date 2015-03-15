87219ddf39fedce2ebcc74c571d316a269405433
Nathan West <nathan.west@okstate.edu>
2015-03-15 19:07:42 -0400

    add news feature and favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,11 +4,11 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Nathan West'
 SITENAME = u'VOLK'
-SITEURL = 'http://nathanwest.us/volk'
+SITEURL = 'http://libvolk.org'
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'US/Central'
 
 DEFAULT_LANG = u'en'
 
@@ -21,15 +21,18 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+#TEMPLATE_PAGES = {'news.html': 'news.html'}
+DIRECT_TEMPLATES = ['index']
+
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
 
